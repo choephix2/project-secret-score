192ba719c7e26893846a051617f9ef15fac182ac
choephix2 <choephix2@gmail.com>
2019-04-23 16:01:45 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+
+var _0x59e1f4 = function( k, d = 0 ) {
+  var o = 6067
+  var m = 7717
+  var v = window[k]
+  v = v ? parseInt( atob( v ) ) - o : 0
+  if ( v % m > 0 ) v = 0
+  v = v / m + d
+  window[k] = btoa( o + v * m )
+  return v
+}
+
+function test( delta=0 )
+{
+  console.log( _0x59e1f4('_0x3353e2',delta), window._0x3353e2 )  
+}
+
+test()
+test()
+test(1)
+test(1)
+test(1)
+test(1)
+test(1)
+test(1)
+test(1)
+test(1)
+test(1)
+test(1)
+test(1)
+test(1)
+test(1)
+test(1)
+test(1)
+test(1)
+test(1)
+test(1)
+test(1)
+test(1)
+test(100)
+test(1000)
+test(10000)
+test()
+test()
---
+++ indiano.js
@@ -0,0 +1,14 @@
+
+var _0x59e1f4 = function( d = 0 ) {
+    var v = window._0x3353e2
+    v = parseInt( v ? atob( v ) : 0 ) + parseInt( d )
+    window._0x3353e2 = btoa( v )
+    return v
+}
+
+console.log( _0x59e1f4() )
+console.log( _0x59e1f4(4) )
+console.log( _0x59e1f4(4) )
+console.log( _0x59e1f4(4) )
+console.log( _0x59e1f4(4) )
+console.log( _0x59e1f4() )
